4493a8d4ab18c6e7241eb6af01d85a0fc7007f83
amasullo12 <118211127+amasullo12@users.noreply.github.com>
2022-12-03 16:06:44 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-console.log("How we doin?");
+var text1 = "Hello";
+var text2 = "Hello, spaces are also OK!";
+var text3 = 'Single quotes are valid too.';
+
+var escapedText1 = "He said: \"Yes!\", that's for sure";
+var escapedText2 = 'He said: "Yes!", that\'s for sure';
+
+var numberInteger = 456;
+var numberInteger2 = -345; // This is negative integer
+var numberFloat = 50.25;
+
+var boolean1 = true;
+var boolean2 = false;
+var notABoolean = "false"; //This is just the string "false", not false!
+
+var emptyVariable = null;
+var notNullButAString = "null";
+console.log(doesntExist); // Outputs undefined
+console.log(emptyVariable); // Outputs Null
+console.log(notNullButAString); // Outputs "null" - not the same as null!
